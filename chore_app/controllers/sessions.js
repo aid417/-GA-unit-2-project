142529bb03be890c698f5cf035ae537963042efa
Alice D'Arcangelo <alicedarcangelo@Alices-MacBook-Pro.local>
2019-11-05 14:36:25 -0500

    can create user and sign in/out

---
+++ chore_app/controllers/sessions.js
@@ -0,0 +1,30 @@
+// DEPENDENCIES
+const express = require("express");
+const router = express.Router();
+const User = require("../models/users.js");
+const bcrypt = require("bcrypt");
+//ROUTES
+router.post("/", (req, res) => {
+  User.findOne({ username: req.body.username }, (err, foundUser) => {
+    if (bcrypt.compareSync(req.body.password, foundUser.password)) {
+      console.log("found", foundUser);
+      req.session.currentUser = foundUser;
+      res.redirect("/");
+    } else {
+      res.send("wrong password");
+    }
+  });
+});
+
+router.get("/new", (req, res) => {
+  res.render("sessions/new.ejs");
+});
+
+router.delete("/", (req, res) => {
+  req.session.destroy(() => {
+    res.redirect("/");
+  });
+});
+
+// EXPORT
+module.exports = router;
--- chore_app/server.js
+++ chore_app/server.js
@@ -4,12 +4,13 @@ const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
 const session = require("express-session");
-// const methodOverride = require("method-override");
+const User = require("./models/users.js");
+const methodOverride = require("method-override");
 //MIDDLEWARE
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static(__dirname + "/public"));
-// app.use(methodOverride("_method"));
+app.use(methodOverride("_method"));
 
 //CONTROLLERS
 app.use(
@@ -21,13 +22,19 @@ app.use(
 );
 const userController = require("./controllers/users.js");
 app.use("/users", userController);
+const sessionController = require("./controllers/sessions.js");
+app.use("/sessions", sessionController);
 //ROUTES
 
 //index:
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { currentUser: req.session.currentUser });
 });
 //LISTENER
 app.listen(port, () => {
   console.log("listening to" + port);
+});
+mongoose.connect("mongodb://localhost:27017/chores");
+mongoose.connection.once("open", () => {
+  console.log("connected to mongo");
 });
